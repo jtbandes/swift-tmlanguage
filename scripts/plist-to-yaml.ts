b788c77884e6f480bf3f21470a2972bb18ebb004
Jacob Bandes-Storch <jacob@bandes-stor.ch>
2023-11-04 00:30:02 -0700

    Hoist all repository patterns to root repository
    Fixes https://github.com/swift-server/vscode-swift/issues/366

--- scripts/plist-to-yaml.ts
+++ scripts/plist-to-yaml.ts
@@ -6,8 +6,23 @@ import { program } from "commander";
 import path from "node:path";
 
 function compareMapKeys(key1: unknown, key2: unknown): number {
-  if (key1 instanceof Scalar && typeof key1.value === "string" && key2 instanceof Scalar && typeof key2.value === "string") {
-    const keyOrder = ["name", "match", "captures", "begin", "end", "beginCaptures", "endCaptures", "patterns", "repository"];
+  if (
+    key1 instanceof Scalar &&
+    typeof key1.value === "string" &&
+    key2 instanceof Scalar &&
+    typeof key2.value === "string"
+  ) {
+    const keyOrder = [
+      "name",
+      "match",
+      "captures",
+      "begin",
+      "end",
+      "beginCaptures",
+      "endCaptures",
+      "patterns",
+      "repository",
+    ];
     const i1 = keyOrder.indexOf(key1.value);
     const i2 = keyOrder.indexOf(key2.value);
     return i1 - i2;
@@ -15,7 +30,7 @@ function compareMapKeys(key1: unknown, key2: unknown): number {
   return 0;
 }
 
-function fixWhitespace(str: string): string {
+function fixWhitespace(str: string, opts: { ignoreFirstLineLeadingWhitespace: boolean }): string {
   let result = str.replaceAll("\t", "  ");
 
   // trimEnd is a workaround for yaml.stringify introducing strange escaped spaces, i.e.:
@@ -25,7 +40,10 @@ function fixWhitespace(str: string): string {
 
   // De-indent by trimming leading spaces that are common to all lines
   let leadingSpaces = Infinity;
-  for (const match of result.matchAll(/^\s+/gm)) {
+  const lines = opts.ignoreFirstLineLeadingWhitespace
+    ? result.replace(/^\S.*\n/, "").matchAll(/^\s+/gm)
+    : result.matchAll(/^\s*/gm);
+  for (const match of lines) {
     leadingSpaces = Math.min(leadingSpaces, match[0].length);
   }
   if (isFinite(leadingSpaces)) {
@@ -35,8 +53,115 @@ function fixWhitespace(str: string): string {
   return result;
 }
 
-async function main({ input, output }: { input: string; output: string }) {
-  const tmLang = plist.parse(await fs.readFile(input, "utf8"));
+type TmLanguagePattern = {
+  include?: string;
+  name?: string;
+  match?: string;
+  captures?: Record<string, TmLanguagePattern>;
+  begin?: string;
+  beginCaptures?: Record<string, TmLanguagePattern>;
+  end?: string;
+  endCaptures?: Record<string, TmLanguagePattern>;
+  repository?: Record<string, TmLanguagePattern>;
+  patterns?: TmLanguagePattern[];
+};
+type TmLanguage = {
+  patterns?: TmLanguagePattern[];
+  repository?: Record<string, TmLanguagePattern>;
+};
+
+function* traverseTmLanguagePatternsDepthFirst(
+  lang: TmLanguage,
+): Generator<{ pattern: TmLanguagePattern; path: string[] }> {
+  function* traverse(
+    pattern: TmLanguagePattern,
+    path: string[],
+  ): Generator<{ pattern: TmLanguagePattern; path: string[] }> {
+    for (const capture of Object.values(pattern.captures ?? {})) {
+      yield* traverse(capture, path);
+    }
+    for (const capture of Object.values(pattern.beginCaptures ?? {})) {
+      yield* traverse(capture, path);
+    }
+    for (const capture of Object.values(pattern.endCaptures ?? {})) {
+      yield* traverse(capture, path);
+    }
+    for (const [name, child] of Object.entries(pattern.repository ?? {})) {
+      yield* traverse(child, [...path, name]);
+    }
+    for (const child of pattern.patterns ?? []) {
+      yield* traverse(child, path);
+    }
+    yield { pattern, path };
+  }
+  for (const pattern of lang.patterns ?? []) {
+    yield* traverse(pattern, []);
+  }
+  for (const [name, pattern] of Object.entries(lang.repository ?? {})) {
+    yield* traverse(pattern, [name]);
+  }
+}
+
+async function main({
+  input,
+  output,
+  hoistRepositories,
+}: {
+  input: string;
+  output: string;
+  hoistRepositories: boolean;
+}) {
+  const tmLang = plist.parse(await fs.readFile(input, "utf8")) as TmLanguage;
+
+  hoistRepositories: if (hoistRepositories) {
+    const rootRepository = { ...tmLang.repository };
+
+    const oldToNewName = new Map<string, string>();
+    for (const { pattern, path } of traverseTmLanguagePatternsDepthFirst(tmLang)) {
+      for (const [name, child] of Object.entries(pattern.repository ?? {})) {
+        const newName = [...path, name].join("-");
+        if (oldToNewName.has(name)) {
+          console.warn(`Unable to hoist repositories due to duplicate name "${newName}"`);
+          break hoistRepositories;
+        }
+        if (rootRepository[newName] != undefined) {
+          console.warn(
+            `Unable to hoist repositories: would rename "${name}" to "${newName}" which conflicts with an existing root repository pattern`,
+          );
+          break hoistRepositories;
+        }
+        oldToNewName.set(name, newName);
+      }
+    }
+
+    for (const { pattern } of traverseTmLanguagePatternsDepthFirst(tmLang)) {
+      // Replace include references
+      if (pattern.include != undefined) {
+        const match = pattern.include.match(/#([\w-]+)$/);
+        if (match?.[1] != undefined) {
+          const newName = oldToNewName.get(match[1]);
+          if (newName != undefined) {
+            pattern.include = pattern.include.substring(0, match.index) + "#" + newName;
+          }
+        }
+      }
+
+      // Move repository patterns to the root repository
+      for (const [name, child] of Object.entries(pattern.repository ?? {})) {
+        const newName = oldToNewName.get(name);
+        if (newName == undefined) {
+          continue;
+        }
+        rootRepository[newName] = child;
+      }
+      delete pattern.repository;
+    }
+
+    tmLang.repository = Object.fromEntries(
+      Object.entries(rootRepository).sort(([a], [b]) => (a < b ? -1 : a > b ? 1 : 0)),
+    );
+  }
+
   const doc = new yaml.Document(tmLang);
 
   yaml.visit(doc, {
@@ -44,7 +169,7 @@ async function main({ input, output }: { input: string; output: string }) {
       // Remove unnecessary indentation from multiline strings
       if (key !== "key" && typeof node.value === "string" && node.value.includes("\n")) {
         node.type = "BLOCK_LITERAL";
-        node.value = fixWhitespace(node.value);
+        node.value = fixWhitespace(node.value, { ignoreFirstLineLeadingWhitespace: false });
       }
 
       // Replace `"0": ...` keys with `0: ...` for clarity
@@ -60,7 +185,9 @@ async function main({ input, output }: { input: string; output: string }) {
       const comment = node.get("comment");
       if (typeof comment === "string") {
         node.delete("comment");
-        node.commentBefore = fixWhitespace(comment).trim().replaceAll(/^/gm, " ");
+        node.commentBefore = fixWhitespace(comment, { ignoreFirstLineLeadingWhitespace: true })
+          .trim()
+          .replaceAll(/^/gm, " ");
       }
 
       // Put things like "0: { name: xyz }" onto a single line
@@ -94,7 +221,9 @@ async function main({ input, output }: { input: string; output: string }) {
           item.key instanceof Scalar &&
           typeof item.key.value === "string" &&
           item.value instanceof YAMLMap &&
-          item.value.items.every(({ key }) => key instanceof Scalar && typeof key.value !== "number") &&
+          item.value.items.every(
+            ({ key }) => key instanceof Scalar && typeof key.value !== "number",
+          ) &&
           idx !== 0
         ) {
           item.key.spaceBefore = true;
@@ -103,9 +232,10 @@ async function main({ input, output }: { input: string; output: string }) {
     },
   });
 
-  const result = yaml.stringify(doc);
+  // Use a large lineWidth to avoid line-breaking PLAIN strings that prettier would not break
+  const result = yaml.stringify(doc, { lineWidth: Infinity });
 
-  const options = await prettier.resolveConfig(output);
+  const options = await prettier.resolveConfig(input);
   const formatted = await prettier.format(result, {
     printWidth: 100,
     ...options,
@@ -120,6 +250,11 @@ program
   .description("Convert a .tmLanguage plist to human-friendly YAML")
   .requiredOption("-i, --input <file>", "tmLanguage input file")
   .requiredOption("-o, --output <file>", "yaml output file")
+  .option(
+    "--hoist-repositories",
+    "Hoist all repository definitions to the top level for VS Code compatibility <https://github.com/microsoft/vscode-textmate/issues/140>",
+    false,
+  )
   .action((options) => {
     main(options).catch((err) => {
       console.error(err);
