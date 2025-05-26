70a370e691b93eee99b955417e8ed12b436effb4
Jacob Bandes-Storch <jacob@bandes-stor.ch>
2025-05-25 21:54:35 -0400

    fix type error

--- scripts/yaml-to-json.ts
+++ scripts/yaml-to-json.ts
@@ -6,7 +6,6 @@ import path from "node:path";
 
 async function main({ input, output }: { input: string; output: string }) {
   const doc = yaml.parseDocument(await fs.readFile(input, "utf8"));
-  1 == "2";
   // Convert comments to `comment` keys
   yaml.visit(doc, {
     Node(key, node, path) {
